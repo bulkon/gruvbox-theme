25d7a3fd534e95a42eb9bb14ac4dc9ecda051745
GitHub Action <action@github.com>
2025-01-20 20:41:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/bulkon/gruvboxtheme/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/bulkon/gruvboxtheme/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.bulkon.gruvboxtheme.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bulkon.gruvboxtheme.MyBundle
+import com.github.bulkon.gruvboxtheme.services.MyProjectService
 import javax.swing.JButton
 
 
